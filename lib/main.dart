e0a064c30b307439ad60691318930f9e9b65fd84
AhmedMaged1722 <am9351705@gmail.com>
2024-07-24 00:17:37 +0300

    refactor bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:notes_app/Cubit/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/Models/note_model.dart';
 import 'package:notes_app/Views/notes_view.dart';
 import 'package:notes_app/constans/const.dart';
 import 'package:notes_app/simble_bloc_observer.dart';
 
 void main() async {
-  // ignore: await_only_futures
   await Hive.initFlutter();
   Bloc.observer = SimbleBlocObserver();
   Hive.registerAdapter(NoteModelAdapter());
@@ -18,8 +16,6 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
